950e68c10ae635ebcdce66bd0c8c38921d1adf59
Choba <watsit777@gmail.com>
2021-04-24 13:36:59 -0500

    V1

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', points: 1 },
+  { id: 12, name: 'Narco', points: 1 },
+  { id: 13, name: 'Bombasto', points: 1 },
+  { id: 14, name: 'Celeritas', points: 1 },
+  { id: 15, name: 'Magneta', points: 1 },
+  { id: 16, name: 'RubberMan', points: 1 },
+  { id: 17, name: 'Dynama', points: 1 },
+  { id: 18, name: 'Dr IQ', points: 1 },
+  { id: 19, name: 'Magma', points: 1 },
+  { id: 20, name: 'Tornado', points: 1 }
+];
